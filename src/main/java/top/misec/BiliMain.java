7dcf7177ba648e7c534cf65ab0fc30693e6673a4
zjkwdy <3377911508@qq.com>
2021-05-02 11:24:10 +0800

    a

--- src/main/java/top/misec/BiliMain.java
+++ src/main/java/top/misec/BiliMain.java
@@ -8,7 +8,6 @@ import top.misec.task.DailyTask;
 import top.misec.task.ServerPush;
 import top.misec.utils.VersionInfo;
 
-
 /**
  * @author Junzhou Liu
  * @create 2020/10/11 2:29
@@ -22,7 +21,7 @@ public class BiliMain {
             log.info("任务启动失败");
             log.warn("Cookies参数缺失，请检查是否在Github Secrets中配置Cookies参数");
         }
-        //读取环境变量
+        // 读取环境变量
         Verify.verifyInit(args[0], args[1], args[2]);
 
         if (args.length > 4) {
@@ -31,17 +30,11 @@ public class BiliMain {
             ServerVerify.verifyInit(args[3]);
         }
 
-
         VersionInfo.printVersionInfo();
-        //每日任务65经验
+        // 每日任务65经验
         Config.getInstance().configInit();
-        if (!Config.getInstance().isSkipDailyTask()) {
-            DailyTask dailyTask = new DailyTask();
-            dailyTask.doDailyTask();
-        } else {
-            log.info("已开启了跳过本日任务，本日任务跳过（不会发起任何网络请求），如果需要取消跳过，请将skipDailyTask值改为false");
-            ServerPush.doServerPush();
-        }
+        DailyTask dailyTask = new DailyTask();
+        dailyTask.doDailyTask();
     }
 
 }
